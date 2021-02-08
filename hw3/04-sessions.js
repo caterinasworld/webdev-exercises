9860c3fb06020d349cce462531b71fa75eda6c93
Caterina Paun <caterina.paun@gmail.com>
2021-02-07 22:12:45 -0800

    Update: Starter files for Homework 3

--- hw3/04-sessions.js
+++ hw3/04-sessions.js
@@ -5,12 +5,13 @@ const port = process.env.PORT || 5000;
 
 // Add your code below
 
+// Use the express-session module
 app.use(/** ... */);
 
 app.get('/', (req, res) => {
-    // Add your code below
+  // Add your code below
 });
-    
+
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
